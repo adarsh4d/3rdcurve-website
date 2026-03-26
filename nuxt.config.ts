d24cb95d57581f74065abfe2de5481b90f98359e
Adarsh DK <adarsh.dk24012@gmail.com>
2026-03-27 01:57:35 +0530

    add oxlint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  css: ['./app/assets/css/main.css'],
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image'],
+  css: ["./app/assets/css/main.css"],
+  modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
-})
+  icon: {
+    mode: "svg",
+  },
+});
